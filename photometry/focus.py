8bb5fe3e9ad6a905e87a33680c4991112648b04e
Scott <scotthakoda@gmail.com>
2025-06-27 12:39:04 -0700

    alternate method for finding optimal focus; plot points traversing curve a few points past minimum, fit a curve, return vertex

--- photometry/focus.py
+++ photometry/focus.py
@@ -36,8 +36,45 @@ def focus_finder(image1, image2, seen):
     else:
         return image1.focus
 
+def curve_finder(image1, image2, seen, direction=None):
+    
+    if image1.fwhm > image2.fwhm:
+        if direction is None:
+            direction = 'right'
+        if direction == 'left':
+            return curve_helper(image1, image2, seen)
+        focus3 = image2.focus + (image2.focus - image1.focus)
+        image3 = Image(focus3)
+        seen.add(image3)
+        return curve_finder(image2, image3, seen, direction)
+    elif image1.fwhm < image2.fwhm:
+        if direction is None:
+            direction = 'left'
+        if direction == 'right':
+            return curve_helper(image1, image2, seen)
+        focus3 = image1.focus - (image2.focus - image1.focus)
+        image3 = Image(focus3)
+        seen.add(image3)
+        return curve_finder(image3, image1, seen, direction)
+    
+def curve_helper(image1, image2, seen, iterations=2):
+    if iterations > 0:
+        if image1.fwhm > image2.fwhm:
+            focus3 = image1.focus - (image2.focus - image1.focus)
+            image3 = Image(focus3)
+            seen.add(image3)
+            return curve_helper(image3, image1, seen, iterations-1)
+        elif image1.fwhm < image2.fwhm:
+            focus3 = image2.focus + (image2.focus - image1.focus)
+            image3 = Image(focus3)
+            seen.add(image3)
+            return curve_helper(image2, image3, seen, iterations-1)
+    else:
+        return seen
+
     
 import argparse
+import quadratic
 
 def main():
     parser = argparse.ArgumentParser(description="Find optimal focus for images based on FWHM.")
@@ -57,7 +94,24 @@ def main():
     seen.add(image2.focus)
 
     focus = focus_finder(image1, image2, seen)
-    print(f"Optimal focus: {focus}")
+    print(f"Optimal focus: {focus}\n")
+
+    seen2 = set()
+    seen2.add(image1)
+    seen2.add(image2)
+    curve = curve_finder(image1, image2, seen2)
+    curve = sorted(curve, key=lambda img: img.focus)
+    x_values = []
+    y_values = []
+    print("Curve found with the following focus values:")
+    for img in curve:
+        print(f"Focus: {img.focus}, FWHM: {img.fwhm}")
+        x_values.append(img.focus)
+        y_values.append(img.fwhm)
+
+    a, b, c = quadratic.fit_quadratic(x_values, y_values)
+    x_vertex, y_vertex = quadratic.vertex(a, b, c)
+    print(f"Optimal focus: {x_vertex}, FWHM: {y_vertex}")
 
 
 if __name__ == "__main__":

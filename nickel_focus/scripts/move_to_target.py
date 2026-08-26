8846dfd7af98d21092a4908783f23604c24cbb5c
Kyle Westfall <westfall@ucolick.org>
2026-08-26 16:48:41 -0700

    udpate slewing to nearest pointing/focus star

--- nickel_focus/scripts/move_to_target.py
+++ nickel_focus/scripts/move_to_target.py
@@ -1,125 +1,52 @@
-#! @KPYTHON@
-import ktl
 
-from astropy.coordinates import EarthLocation, SkyCoord
-from astropy.time import Time 
-from astropy import units as u
-import datetime
-import numpy as np
-
-import warnings
-warnings.filterwarnings("ignore", message="Input line .* contained no data")
-
-def wait_until(self, keyword, expected_value, timeout=15):
-        start_time = time.time()
-        while time.time() - start_time < timeout:
-            if keyword.read() in expected_value:
-                return True
-        return False
-
-def find_nearest_star(telescope, star_type, file):
-
-    if star_type == 'focus':
-        print("Finding nearest focus star...")
-        star_type = 'Pointing'
-    elif star_type == 'pointing':
-        print("Finding nearest pointing star...")
-        star_type = 'Focusing'
-    else:
-        raise ValueError("Invalid star type. Choose either 'focus' or 'pointing'.")
-    
-
-    min_separation = float('inf')
-    nearest_star = None
-
-    focus_stars = np.loadtxt(file, dtype=str, usecols=(0, 1, 2), comments=star_type)
-
-    focus_coords = SkyCoord(ra=focus_stars[:, 1], dec=focus_stars[:, 2], unit=('hourangle', 'deg'))
-
-    separations = telescope.separation(focus_coords)
-
-    min_index = np.argmin(separations)
-    nearest_star = focus_stars[min_index]
-
-    target_name = nearest_star[0]
-    target_ra = nearest_star[1]
-    target_dec = nearest_star[2]
-
-    print(f'Target Name: {target_name}')
-    print(f'Target RA: {target_ra}')
-    print(f'Target DEC: {target_dec}')
-
-    return target_name, target_ra, target_dec
+from nickel_focus.scripts import scriptbase
 
 
-def move_to_target(target_coords):
-    stop_key = ktl.cache('nickelpoco', 'POCSTOP')
-    target_key = ktl.cache('nickelpoco', 'POCOT')
-    track_key = ktl.cache('nickelpoco', 'POCTRCK')
-    ra_desired = ktl.cache('nickelpoco', 'POCRAD')
-    dec_desired = ktl.cache('nickelpoco', 'POCDECD')
+class NickelSlewToNearest(scriptbase.ScriptBase):
 
-    if stop_key.read() != 0:
-        print("POCSTOP is 'disabled'. Waiting for 'allowed' to allow motion")
-    if not wait_until(stop_key, 'allowed', timeout=30):
-        raise Exception("POCSTOP is 'disabled'. Set to 'allowed' to allow motion")
+    @classmethod
+    def get_parser(cls, width=None):
+        parser = super().get_parser(
+            description='Slew the telescope to the nearest target', width=width
+        )
 
-    if track_key.read() == 'off':
-        raise Exception("Tracking is not enabled. Please enable tracking before moving to target.")
-        # 0->off, 1->on
+        parser.add_argument(
+            '-f', '--starlist', default=None, type=str,
+            help=(
+                'Name of a starlist file with the possible targets.  If not provided, defaults '
+                'to the list of pointing and focusing stars.'
+            )
+        )
+        parser.add_argument(
+            '-s', '--search', default=None,
+            help=(
+                'Search string used to down-select the target list.  For example, to find the '
+                'nearest pointing star in the default star list, use "-s Pointing".'
+            )
+        )
+        parser.add_argument(
+            '--dry_run', default=False, action='store_true',
+            help=(
+                'Run in dry run mode; i.e., the script will print the nearest object, but the '
+                'telescope will not move.'
+            )
+        )
+        return parser
 
-    if target_key.read() != '0':
-        print("Telescope not ready to move to target. Waiting for ready (POCOT to be 0)")
-        # 1->have not reached target, 0-> on target and stable, -1-> failed to reach target within time limit and not on target.
-    if not wait_until(target_key, '0', timeout=30):
-        raise Exception("Telescope is not ready to move to target. Please wait until the telescope is ready.")
+    @classmethod
+    def main(cls, args):
 
-    ra_desired.write(target_coords.ra.to('hourangle').value)
-    dec_desired.write(target_coords.dec.to('deg').value)
+        from nickel_focus import log
+        from nickel_focus import slew
 
-    if wait_until(target_key, '0', timeout=60):
-        print("Telescope is now on target.")
-    else:
-        if target_key.read() == '-1':
-            raise Exception("Failed to reach target within time limit. Telescope is not on target.")
-        else:
-            raise Exception("Telescope is not on target. Please check the status.")
+        cls.init_log(args)
 
-# POCOBJNA = Pointing16
+        telescope = slew.NickelTelescopePointing()
 
-import argparse
+        target_name, target_ra, target_dec = slew.find_nearest_star(
+            telescope.current, obj_search_str=args.search, file=args.starlist
+        )
+        log.info(f'Nearest object is: {target_name} at RA={target_ra}, Dec={target_dec}')
 
-def main():
-    parser = argparse.ArgumentParser(description='Move telescope to target')
-    parser.add_argument('--dry_run', action='store_true', help='Run in dry run mode (no actual movement)')
-    parser.add_argument('--star_type', choices=['focus', 'pointing'], default='focus', help='Type of star to use for target selection (focus or pointing)')
-    args = parser.parse_args()
-
-    lick = EarthLocation.of_site('Lick Observatory')
-    print(lick)
-
-    t = Time(datetime.datetime.now(datetime.UTC), location=lick)
-    print(f'Current time: {t}')
-
-    lst = t.sidereal_time('mean')
-
-    ra_key = ktl.cache('nickelpoco', 'POCORAA')
-    dec_key = ktl.cache('nickelpoco', 'POCODECA')
-    lick_ra = ra_key.read()
-    lick_dec = dec_key.read()
-    print(f'Lick RA: {lick_ra}')
-    print(f'Lick DEC: {lick_dec}')
-
-    telescope_coord = SkyCoord(ra=lick_ra, dec=lick_dec, unit=('hourangle', 'deg'))
-
-    target_name, target_ra, target_dec = find_nearest_star(telescope_coord, args.star_type, 'point_focus.txt')
-
-    target_coords = SkyCoord(ra=target_ra, dec=target_dec, unit=('hourangle', 'deg'))
-
-    if not args.dry_run:
-        move_to_target(target_coords)
-
-if __name__ == '__main__':
-    main()
-
-# note: if first time acquiring target and not idling on pocot=0, it will timeout waiting for pocot=0 unless the user moves to a target and is successful within the timeout period.
+        if not args.dry_run:
+            telescope.slew_to(target_ra, target_dec)
--- nickel_focus/slew.py
+++ nickel_focus/slew.py
@@ -1,10 +1,9 @@
-import datetime
+
 from importlib import resources
 from pathlib import Path
 import warnings
 
-from astropy.coordinates import EarthLocation, SkyCoord
-from astropy.time import Time 
+from astropy.coordinates import SkyCoord
 import numpy as np
 
 try:
@@ -14,9 +13,10 @@ except ModuleNotFoundError:
     ktl = None
 
 from nickel_focus import log
+from nickel_focus import starlist
 
 
-class TelescopePointing:
+class NickelTelescopePointing:
     """
     KTL object used to manipulate the telescope pointing.
     """
@@ -42,6 +42,10 @@ class TelescopePointing:
         # Sets the desired Dec of the telescope pointing
         self.decd = ktl.cache('nickelpoco', 'POCODECD') 
 
+    @property
+    def current(self):
+        return SkyCoord(ra=self.raa.read(), dec=self.deca.read(), unit=('hourangle', 'deg'))
+
     def slew_to(self, ra, dec):
 
         if not self.pocstop.waitFor('== allowed', timeout=0.5):
@@ -66,60 +70,19 @@ class TelescopePointing:
 
 
 def find_nearest_target(telescope_coo, obj_search_str=None, file=None):
-
     _file = (
         resources.files('nickel_focus') / 'data' / 'point_focus.txt'
         if file is None else Path(file).absolute()
     )
-    coo_tbl = np.loadtxt(_file, dtype=str)
+    tbl = starlist.parse_starlist(_file)
 
     if obj_search_str is not None:
-        selected = [obj_search_str in obj_name for obj_name in coo_tbl[:,0]]
+        selected = [obj_search_str in obj_name for obj_name in tbl['name']]
         if np.sum(selected) == 0:
             raise ValueError(f'{_file} has no object names containing {obj_search_str}')
-        coo_tbl = coo_tbl[selected]
+        tbl = tbl[selected]
 
-    obj_coo = SkyCoord(ra=coo_tbl[:,1], dec=coo_tbl[:,2], unit=('hourangle', 'deg'))
+    obj_coo = SkyCoord(ra=tbl['ra'], dec=tbl['dec'], unit=('hourangle', 'deg'))
     offsets = telescope_coo.separation(obj_coo)
-
-    nearest_obj = coo_tbl[np.argmin(offsets)]
-    log.info(f'Nearest object is: {nearest_obj[0]} at RA={nearest_obj[1]}, Dec={nearest_obj[2]}')
-    return nearest_obj[0], nearest_obj[1], nearest_obj[2]
-
-
-import argparse
-
-def main():
-    parser = argparse.ArgumentParser(description='Move telescope to target')
-    parser.add_argument('--dry_run', action='store_true', help='Run in dry run mode (no actual movement)')
-    parser.add_argument('--star_type', choices=['focus', 'pointing'], default='focus', help='Type of star to use for target selection (focus or pointing)')
-    args = parser.parse_args()
-
-    lick = EarthLocation.of_site('Lick Observatory')
-    print(lick)
-
-    t = Time(datetime.datetime.now(datetime.UTC), location=lick)
-    print(f'Current time: {t}')
-
-    lst = t.sidereal_time('mean')
-
-    ra_key = ktl.cache('nickelpoco', 'POCORAA')
-    dec_key = ktl.cache('nickelpoco', 'POCODECA')
-    lick_ra = ra_key.read()
-    lick_dec = dec_key.read()
-    print(f'Lick RA: {lick_ra}')
-    print(f'Lick DEC: {lick_dec}')
-
-    telescope_coord = SkyCoord(ra=lick_ra, dec=lick_dec, unit=('hourangle', 'deg'))
-
-    target_name, target_ra, target_dec = find_nearest_star(telescope_coord, args.star_type, 'point_focus.txt')
-
-    target_coords = SkyCoord(ra=target_ra, dec=target_dec, unit=('hourangle', 'deg'))
-
-    if not args.dry_run:
-        move_to_target(target_coords)
-
-if __name__ == '__main__':
-    main()
-
-# note: if first time acquiring target and not idling on pocot=0, it will timeout waiting for pocot=0 unless the user moves to a target and is successful within the timeout period.
+    nearest_obj = tbl[np.argmin(offsets)]
+    return nearest_obj['name'][0], nearest_obj['ra'][0], nearest_obj['dec'][0]
